117032f80471bf3501d92bce0f317632b1b28281
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-24 10:48:56 -0600

    Created epic template

--- epic/epic.php
+++ epic/epic.php
@@ -6,5 +6,28 @@
 	</head>
 	<body>
 
+		<h1>Epic</h1>
+
+		<h2>Personas</h2>
+		<p>Text here.</p>
+
+		<h2>Use Cases</h2>
+		<p>Text here.</p>
+
+		<h2>Conceptual Schema</h2>
+		<p>Text here.</p>
+
+		<h2>ERDs</h2>
+		<p>Text here.</p>
+
+		<h2>System Summary</h2>
+		<p>Text here.</p>
+
+		<h2>Development Roadmap</h2>
+		<p>Text here.</p>
+
+		<h2>User Stories</h2>
+		<p>Text here.</p>
+
 	</body>
 </html>
