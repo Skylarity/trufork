9332db9f529d03692b696e8be1d7082de822b440
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-24 12:01:00 -0600

    Added a whole dang user case

--- epic/epic.php
+++ epic/epic.php
@@ -6,28 +6,49 @@
 	</head>
 	<body>
 
+		<!-- Epic documentation -->
 		<h1>Epic</h1>
 
+		<!-- Personas -->
 		<h2>Personas</h2>
+
 		<p>Text here.</p>
 
+		<!-- Use Cases -->
 		<h2>Use Cases</h2>
+
 		<p>Text here.</p>
 
+		<!-- Conceptual Schema -->
 		<h2>Conceptual Schema</h2>
+
 		<p>Text here.</p>
 
+		<!-- ERDs -->
 		<h2>ERDs</h2>
+
 		<p>Text here.</p>
 
+		<!-- System Summary -->
 		<h2>System Summary</h2>
+
 		<p>Text here.</p>
 
+		<!-- Development Roadmap -->
 		<h2>Development Roadmap</h2>
+
 		<p>Text here.</p>
 
+		<!-- User Stories -->
 		<h2>User Stories</h2>
-		<p>Text here.</p>
+
+		<p>
+			I am a user, and I want to find a <em>clean</em> restaurant to eat at.
+		</p>
+
+		<p>
+			Text here.
+		</p>
 
 	</body>
 </html>
