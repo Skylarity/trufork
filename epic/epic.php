057daca7c9de40976485967eb20072655287946d
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-27 14:51:21 -0600

    Conceptual Schema

--- epic/epic.php
+++ epic/epic.php
@@ -1,81 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8">
+	<head>
+		<meta charset="UTF-8">
 
-      <!-- CSS -->
-      <link rel="stylesheet" type="text/css" href="../css/epic.css">
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/epic.css">
 
-      <!-- Font Awesome -->
-      <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-      <!-- Google Fonts -->
-      <link
-         href="https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic,100,100italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700"
-         rel="stylesheet" type="text/css">
+		<!-- Google Fonts -->
+		<link
+			href="https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic,100,100italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700"
+			rel="stylesheet" type="text/css">
 
-      <link type="image/ico" href="../images/fork.ico" rel="shortcut icon"/>
-      <!-- lame favicon generated using GIMP -->
+		<link type="image/ico" href="../images/fork.ico" rel="shortcut icon"/>
+		<!-- lame favicon generated using GIMP -->
 
-      <title>Epic: Documentation</title>
-   </head>
-   <body class="global">
+		<title>Epic: Documentation</title>
+	</head>
+	<body class="global">
 
-      <!-- Epic documentation -->
-      <header class="title">
-         <h1 class="container">
-            <img class="logo" src="../images/fork-blue.svg" alt="TruFork Logo"> <em class="color-blue">TruFork</em>
-            Documentation: <span class="thin-title">Epic</span>
-         </h1>
-      </header>
+		<!-- Epic documentation -->
+		<header class="title">
+			<h1 class="container">
+				<img class="logo" src="../images/fork-blue.svg" alt="TruFork Logo"> <em class="color-blue">TruFork</em>
+				Documentation: <span class="thin-title">Epic</span>
+			</h1>
+		</header>
 
-      <!-- System Goals -->
-      <div class="full-width padding-top-50">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-check-square-o"></i> System Goals:</h2>
+		<!-- System Goals -->
+		<div class="full-width padding-top-50">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-check-square-o"></i> System Goals:</h2>
 
-            <p>
-               A major goal of this project is to provide a way for people to easily find a delicious, <em>clean</em>
-               place to eat.
-            </p>
+				<p>
+					A major goal of this project is to provide a way for people to easily find a delicious,
+					<em>clean</em>
+					place to eat.
+				</p>
 
-            <p>
-               A secondary goal is for restaurant owners to be able to turn their establishment's cleanliness into a
-               marketable trait.
-            </p>
+				<p>
+					A secondary goal is for restaurant owners to be able to turn their establishment's cleanliness into
+					a
+					marketable trait.
+				</p>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-      <!-- Personas - K -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-male"></i> Personas:</h2>
+		<!-- Personas - K -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-male"></i> Personas:</h2>
 
-            <p>TBecky
-               Becky is a 32 year old female with a family. Her family includes her husband and two children ages 7 and 10.
-               The family is middle class working family. Becky is looking for an easy way to find a restaurant to feed her family.
-               She is concerned about quality, location, price, popularity and cleanliness. She accesses social networking sites
-               daily and values the opinion of others.  Becky and her family eat out biweekly on the weekends.
-               She once got food poisoning from a restaurant that had been downgraded and expects a approval from city expectations.
-               She access TRUfork through the web and makes a decision based on the core data provided from TRUfork.
-               This ensures that she's getting the best quality, price and atmosphere for her dollar.
-               .</p>
+				<p>TBecky
+					Becky is a 32 year old female with a family. Her family includes her husband and two children ages 7
+					and 10.
+					The family is middle class working family. Becky is looking for an easy way to find a restaurant to
+					feed her family.
+					She is concerned about quality, location, price, popularity and cleanliness. She accesses social
+					networking sites
+					daily and values the opinion of others. Becky and her family eat out biweekly on the weekends.
+					She once got food poisoning from a restaurant that had been downgraded and expects a approval from
+					city expectations.
+					She access TRUfork through the web and makes a decision based on the core data provided from
+					TRUfork.
+					This ensures that she's getting the best quality, price and atmosphere for her dollar.
+					.</p>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-      <!-- Use Cases - K -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-suitcase"></i> Use Cases:</h2>
+		<!-- Use Cases - K -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-suitcase"></i> Use Cases:</h2>
 
-            <ul>
+				<ul>
 					<li>User access TRUfork.com using a web browser</li>
-               <li>has an option to sign in and/or create a user name</li>
-					<li>First,  Last Name</li>
+					<li>has an option to sign in and/or create a user name</li>
+					<li>First, Last Name</li>
 					<li>Email</li>
 					<li>Password</li>
 					<li>User taps or clicks on search field</li>
@@ -86,168 +93,196 @@
 					<li>food type</li>
 					<li>phone</li>
 					<li>city data approved or disapproved</li>
-					<li>User submits query. The search cross references Googles Places API and City of Albuquerque Data</li>
+					<li>User submits query. The search cross references Googles Places API and City of Albuquerque
+						Data
+					</li>
 					<li>The user sees returned results in the form of TRUfork ratings. 1-5 and a TRUfork approval</li>
 					<li>TRUfork approval includes city data and averages GOOGLE data.</li>
-					<li>The user has a choice to save the results if signed in or has the option to sign up the same as step I</li>
+					<li>The user has a choice to save the results if signed in or has the option to sign up the same as
+						step I
+					</li>
 					<li>The user man click to find out more information</li>
 					<li>Map</li>
 					<li>Address</li>
 					<li>Phone</li>
 					<li>Other User Ratings</li>
 					<li>They may TRUfork it (like)</li>
-					<li>	Comment</li>
+					<li> Comment</li>
 					<li>Compare field</li>
-            </ul>
+				</ul>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-      <!-- Conceptual Schema - S -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-cog"></i> Conceptual Schema:</h2>
+		<!-- Conceptual Schema - S -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-cog"></i> Conceptual Schema:</h2>
 
-            <p>A user has an ID and a password hash. One user has one profile. Many profiles can vote, like, and comment on many restaurants, and friend many other profiles. A restaurant has an ID, Google's restaurant ID, Google's rating, ABQ's restaurant ID, ABQ's inspection data, our TruFork rating, and an address.</p>
+				<p>
+					A user has an ID and a password hash. One user has one profile.
+				</p>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<p>
+					A profile has a profile ID, a user ID, and an email. Many profiles can vote, like, and comment on
+					many restaurants, and friend many other profiles.
 
-      <!-- ERDs - S -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-sitemap"></i> Entity Relationship Diagram:</h2>
+				<p>
+					A restaurant has an ID, Google's restaurant ID, Google's rating, ABQ's restaurant ID, ABQ's
+					inspection data, our TruFork rating, and an address.
+				</p>
+				<p>
+					A comment has a comment ID, a profile ID, a restaurant ID, a date posted, and the actual comment content.
+				</p>
 
-            <img src="../images/trufork_erd.svg" alt="TruFork ERD"/>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+		<!-- ERDs - S -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-sitemap"></i> Entity Relationship Diagram:</h2>
 
-      <!-- System Summary - T -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-terminal"></i> System Summary:</h2>
+				<img src="../images/trufork_erd.svg" alt="TruFork ERD"/>
 
-            <p>
-               TruFork is a website that bridges two different databases containing related information. In essence,
-               TruFork serves as a mechanism to mesh data from Google Places and the City of Albuquerque's ABQ Data
-               portal. The end result is to combine Google's restaurant reviews with ABQ Data's restaurant inspection
-               reports.
-            </p>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-            <p>
-               To create this nexus of data, TruFork will exist initially as a website<sup><a href="#fn1"
-                                                                                              id="ref1">1</a></sup>,
-               utilizing HTML, CSS, JavaScript, and PHP. TruFork will employ scheduled data-gathering and caching
-               algorithms to acquire current Albuquerque restaurant inspection reports. Using the Google Places
-               application programming interface (API), TruFork will index restaurant address information against the
-               database generated from ABQ Data's restaurant inspection reports. In so doing, TruFork will permit its
-               users to read restaurant reviews <em>and</em> restaurant inspection reports in one location, <em>seamlessly</em>.
-            </p>
+		<!-- System Summary - T -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-terminal"></i> System Summary:</h2>
 
-            <p>
-               TruFork will also offer a slim, nominal social media user interface. TruFork's users will be able to
-               generate user accounts, generate and read comments, select favorite establishments, and be alerted when
-               new restaurant inspection reports are made regarding specific restaurants.
-            </p>
+				<p>
+					TruFork is a website that bridges two different databases containing related information. In
+					essence,
+					TruFork serves as a mechanism to mesh data from Google Places and the City of Albuquerque's ABQ Data
+					portal. The end result is to combine Google's restaurant reviews with ABQ Data's restaurant
+					inspection
+					reports.
+				</p>
 
-            <br/> <!-- BrBa -->
+				<p>
+					To create this nexus of data, TruFork will exist initially as a website<sup><a href="#fn1"
+																								   id="ref1">1</a></sup>,
+					utilizing HTML, CSS, JavaScript, and PHP. TruFork will employ scheduled data-gathering and caching
+					algorithms to acquire current Albuquerque restaurant inspection reports. Using the Google Places
+					application programming interface (API), TruFork will index restaurant address information against
+					the
+					database generated from ABQ Data's restaurant inspection reports. In so doing, TruFork will permit
+					its
+					users to read restaurant reviews <em>and</em> restaurant inspection reports in one location, <em>seamlessly</em>.
+				</p>
 
-            <hr/>
-            <!-- Yep, PHPStorm, it's empty. Thanks.-->
+				<p>
+					TruFork will also offer a slim, nominal social media user interface. TruFork's users will be able to
+					generate user accounts, generate and read comments, select favorite establishments, and be alerted
+					when
+					new restaurant inspection reports are made regarding specific restaurants.
+				</p>
 
-            <sup id="fn1">1. Future plans for TruFork include mobile apps across all extant platforms with significant
-               userbase.<a href="#ref1" title="Jump back to footnote 1 in the text."> <i
-                     class="fa fa-arrow-circle-o-up"></i></a></sup>
+				<br/> <!-- BrBa -->
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<hr/>
+				<!-- Yep, PHPStorm, it's empty. Thanks.-->
 
-      <!-- Development Roadmap - T -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-road"></i> Development Roadmap:</h2>
+				<sup id="fn1">1. Future plans for TruFork include mobile apps across all extant platforms with
+					significant
+					userbase.<a href="#ref1" title="Jump back to footnote 1 in the text."> <i
+							class="fa fa-arrow-circle-o-up"></i></a></sup>
 
-            <p>
-               CAUTION: Development is not linear. As such, this roadmap will likely be deviated from, built upon,
-               circumvented, folded, spindled, and mutilated.
-            </p>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-            <br/> <!-- Breakin' 2: Electric Boogaloo-->
+		<!-- Development Roadmap - T -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-road"></i> Development Roadmap:</h2>
 
-            <em>User Interface Features</em>
-            <ul>
-               <li>Basic, minimalist portal with aesthetically-pleasing graphics</li>
-               <li>Update field including new inspection data</li>
-               <li>Update field for friend activity</li>
-               <li>User log-in field</li>
-               <li>Search field</li>
-            </ul>
+				<p>
+					CAUTION: Development is not linear. As such, this roadmap will likely be deviated from, built upon,
+					circumvented, folded, spindled, and mutilated.
+				</p>
 
-            <br/> <!-- I breaka you face! -->
+				<br/> <!-- Breakin' 2: Electric Boogaloo-->
 
-            <em>Backend Features</em>
-            <ul>
-               <li>Database culled from weekly download from ABQ Data</li>
-               <li>Google Places API</li>
-            </ul>
+				<em>User Interface Features</em>
+				<ul>
+					<li>Basic, minimalist portal with aesthetically-pleasing graphics</li>
+					<li>Update field including new inspection data</li>
+					<li>Update field for friend activity</li>
+					<li>User log-in field</li>
+					<li>Search field</li>
+				</ul>
 
-            <br/> <!-- Give us a break with all the breaks, eh?-->
+				<br/> <!-- I breaka you face! -->
 
-            <em>Social Media Features</em>
-            <ul>
-               <li>Friend Functionality</li>
-               <li>Like Functionality (for liking comments, restaurants, etc...)</li>
-               <li>Invite Friends Functionality (for bringing in friends from other sites)</li>
-               <li>Symbolic Rewards Functionality</li>
-            </ul>
+				<em>Backend Features</em>
+				<ul>
+					<li>Database culled from weekly download from ABQ Data</li>
+					<li>Google Places API</li>
+				</ul>
 
-            <br/> <!-- Breaker 1-9 -->
+				<br/> <!-- Give us a break with all the breaks, eh?-->
 
-            <em>Future Features</em>
-            <ul>
-               <li>Mobile Apps for Android and iOS devices</li>
-            </ul>
+				<em>Social Media Features</em>
+				<ul>
+					<li>Friend Functionality</li>
+					<li>Like Functionality (for liking comments, restaurants, etc...)</li>
+					<li>Invite Friends Functionality (for bringing in friends from other sites)</li>
+					<li>Symbolic Rewards Functionality</li>
+				</ul>
 
-            <div class="hr"></div>
-         </div>
-      </div>
+				<br/> <!-- Breaker 1-9 -->
 
-      <!-- User Stories -->
-      <div class="full-width">
-         <div class="container">
-            <h2 class="thin"><i class="fa fa-users"></i> User Stories:</h2>
+				<em>Future Features</em>
+				<ul>
+					<li>Mobile Apps for Android and iOS devices</li>
+				</ul>
 
-            <ul>
-               <li>
-                  I am a user, and I want <em>not</em> to get food poisoning again.
-               </li>
+				<div class="hr"></div>
+			</div>
+		</div>
 
-               <li>
-                  I am a restaurant owner, and I want the fact that I have a clean restaurant to get out to people.
-               </li>
+		<!-- User Stories -->
+		<div class="full-width">
+			<div class="container">
+				<h2 class="thin"><i class="fa fa-users"></i> User Stories:</h2>
 
-               <li>
-                  I am a restaurant inspector working for a local government, and I want people in the community to take
-                  some ownership of their dining choices.
-               </li>
+				<ul>
+					<li>
+						I am a user, and I want <em>not</em> to get food poisoning again.
+					</li>
 
-               <li>
-                  I am a food columnist/reviewer for a local newspaper, and I want my readers to be able to follow up on
-                  my reviews with their own research.
-               </li>
+					<li>
+						I am a restaurant owner, and I want the fact that I have a clean restaurant to get out to
+						people.
+					</li>
 
-               <li>
-                  I am a traveling businesswoman, and I want to be able to make quick, but educated decisions about
-                  where to eat when I am on the road.
-               </li>
-            </ul>
-         </div>
-      </div>
+					<li>
+						I am a restaurant inspector working for a local government, and I want people in the community
+						to take
+						some ownership of their dining choices.
+					</li>
 
-   </body>
+					<li>
+						I am a food columnist/reviewer for a local newspaper, and I want my readers to be able to follow
+						up on
+						my reviews with their own research.
+					</li>
+
+					<li>
+						I am a traveling businesswoman, and I want to be able to make quick, but educated decisions
+						about
+						where to eat when I am on the road.
+					</li>
+				</ul>
+			</div>
+		</div>
+
+	</body>
 </html>
