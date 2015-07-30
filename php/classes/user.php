6d482bbfdf9e9aafe37f755de1b53bbaa76be40e
Kenneth Chavez <kchavez.bills@gmail.com>
2015-07-30 16:33:27 -0600

    Created the User classes and construct methods Started the accessor methods and mutator methods
    https://app.asana.com/0/41180108506094/43141206387842

--- php/classes/user.php
+++ php/classes/user.php
@@ -24,21 +24,71 @@ class User {
 	private $hash;
 
 	/**
-	 * contructor for userId
+	 * constructor for userId
 	 *
 	 * @param mixed $newUserId id of this user or null if a new user
-	 * @param string $newSalt encrypted 64 hex pa
-	 * @param string $newHash encrypted 128
+	 * @param string $newSalt password encrypted 64 hex pa
+	 * @param string $newHash $password encrypted 128 using pbkdf2 algorithm
+	 * @throw InvalidArgumentException if data types are not valid
 	 * @throw rangeException if values are not === to 128
-	 * */
+	 * @throw Exception if some other exception is thrown
+	 * @throws Exception
+	 */
 
-	public function __construct($newUserId, $newSalt,$newHash){
+	public function __construct($newUserId, $newSalt, $newHash = null) {
 		try {
 			$this->setUserId($newUserId);
 			$this->setSalt($newSalt);
-			$this->SetHash($newHash);
-		} catch(InvalidArgumentException($invalidArgument->getMessages()))
+			$this->setHash($newHash);
+		} catch(InvalidArgumentException $invalidArgument) {
+			//rethrow the exception to the call
+			throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(RangeException $range) {
+			// rethrow the exception to caller
+			throw(new RangeException($range->getMessage(), 0, $range));
+			// rethrow the exception to caller
+		} catch(Exception $exception) {
+			//rethrow generic exception
+			throw(new Exception($exception->getMessage(), 0, $exception));
 		}
 	}
 
+	/**
+	 *accessor method for user id
+	 *
+	 *@return mixed value
+	 **/
+	public function getUserId() {
+		return($this->userId);
 	}
+
+	/**
+	 * mutator method for user id
+	 * @param $newUserId new value for userId
+	 * @throws InvalidArgumentException if $newUserId is not an integer
+	 */
+
+
+	private function setUserId($newUserId) {
+		//base: case: if the user id is null, this a new user id without mySQL assigned id (yet)
+		if($newUserId === null) {
+			$this->userId = null;
+			return;
+		}
+
+	}
+
+	/**
+	 * @param $newSalt
+	 */
+	private function setSalt($newSalt) {
+	}
+
+	/**
+	 * @param $newHash
+	 */
+	private function setHash($newHash) {
+	}
+}
+
+
