5a90c12cb69e1fa21a9b0bfc8bd992563429b00c
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-07-29 23:52:01 -0600

    added $userId, $salt, $hash to class User started the constructor method for class

--- php/classes/user.php
+++ php/classes/user.php
@@ -7,5 +7,38 @@
  * Time: 9:44 PM
  */
 class User {
+	/**
+	 * id for this user is a primary key
+	 * @var int $userId
+	**/
+	private $userId;
+	/**
+	 * salt encryption for userId
+	 * @var int $userId
+	 **/
+	private $salt;
+	/**
+	 * hash encryption for userId
+	 * @var string $hash password
+	 * */
+	private $hash;
 
-}
+	/**
+	 * contructor for userId
+	 *
+	 * @param mixed $newUserId id of this user or null if a new user
+	 * @param string $newSalt encrypted 64 hex pa
+	 * @param string $newHash encrypted 128
+	 * @throw rangeException if values are not === to 128
+	 * */
+
+	public function __construct($newUserId, $newSalt,$newHash){
+		try {
+			$this->setUserId($newUserId);
+			$this->setSalt($newSalt);
+			$this->SetHash($newHash);
+		} catch(InvalidArgumentException($invalidArgument->getMessages()))
+		}
+	}
+
+	}
