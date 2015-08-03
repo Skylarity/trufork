3260a4727f16ab040ab145bd5432c7f3e75ab947
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-03 15:50:51 -0600

    Created inital violation and data downloader classes https://app.asana.com/0/42983347910415/43141204807875 https://app.asana.com/0/42983347910415/43141204807877

---
+++ php/classes/data-downloader.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: skyre_000
+ * Date: 8/3/2015
+ * Time: 15:46
+ */
+class DataDownloader {
+
+
+
+}
---
+++ php/classes/violation.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: skyre_000
+ * Date: 8/3/2015
+ * Time: 15:48
+ */
+class Violation {
+
+}
