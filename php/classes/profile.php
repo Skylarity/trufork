251c66a82ba67998c3a300a9bd8c6269b9aa2e13
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-08-02 20:38:38 -0600

    added getters and setters in Proile class
    https://app.asana.com/0/41180108506094/43141206387842

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -10,17 +10,62 @@ class Profile {
 	/**
 	 * id for TruFork; this is a primary key
 	 * @var	int $profileId
-	 */private profileId;
+	 */private $profileId;
 
 	/**
 	 * user id that saved end user uses to vote
 	 * @var $userId; this is a primary key
 	 **/
-	private userId;
+	private $userId;
 
 	/** email is an attribute
 	 *@var emailId; this is stored in profile settings
 	 **/
+	private $email;
+
+	/**
+	 * accessor method for tweet id
+	 * @return int
+	 */
+	public function getProfileId() {
+		return $this->profileId;
+	}
+
+	/**
+	 * @param int $profileId
+	 */
+	public function setProfileId($profileId) {
+		$this->profileId = $profileId;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUserId() {
+		return $this->userId;
+	}
+
+	/**
+	 * @param mixed $userId
+	 */
+	public function setUserId($userId) {
+		$this->userId = $userId;
+	}
+
+	/**
+	 * @return emailId
+	 */
+	public function getEmail() {
+		return $this->email;
+	}
+
+	/**
+	 * @param emailId $email
+	 */
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+
 
 
 }
