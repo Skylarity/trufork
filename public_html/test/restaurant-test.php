bdca12fae2bcaf4be3f2145e663f5c79810122db
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-06 11:12:59 -0600

    Almost finished with unit test for Restaurant class https://app.asana.com/0/41180108506016/43141204807871

--- public_html/test/restaurant-test.php
+++ public_html/test/restaurant-test.php
@@ -91,7 +91,7 @@ class RestaurantTest extends TruForkTest {
 	 *
 	 * @expectedException PDOException
 	 **/
-	public function testInsertInvalidProfile() {
+	public function testInsertInvalidRestaurant() {
 		// create a profile with a non null profileId and watch it fail
 		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
 		$restaurant->insert($this->getPDO());
@@ -154,6 +154,223 @@ class RestaurantTest extends TruForkTest {
 		$pdoRestaurant = Restaurant::getRestaurantById($this->getPDO(), $restaurant->getRestaurantId());
 		$this->assertNull($pdoRestaurant);
 		$this->assertSame($numRows, $this->getConnection()->getRowCount("restaurant"));
+	}
+
+	/**
+	 * Test deleting a Restaurant that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testDeleteInvalidRestaurant() {
+		// create a Restaurant and try to delete it without actually inserting it
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->delete($this->getPDO());
+	}
+
+	/**
+	 * Test grabbing a Restaurant by Google ID
+	 */
+	public function testGetValidRestaurantByGoogleId() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurant = Restaurant::getRestaurantByGoogleId($this->getPDO(), $restaurant->getGoogleId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+		$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+		$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+		$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+		$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+		$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+		$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+		$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by Google ID that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByGoogleId() {
+		// Grab a Google ID that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, "doesnotexistG3TR3KT", $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$this->assertNull($restaurant);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by facility key
+	 */
+	public function testGetValidRestaurantByFacilityKey() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurant = Restaurant::getRestaurantByFacilityKey($this->getPDO(), $restaurant->getFacilityKey());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+		$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+		$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+		$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+		$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+		$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+		$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+		$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by facility key that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByFacilityKey() {
+		// Grab a facility key that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, "R3KTS0N", $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$this->assertNull($restaurant);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by name
+	 */
+	public function testGetValidRestaurantByName() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurant = Restaurant::getRestaurantByName($this->getPDO(), $restaurant->getName());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+		$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+		$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+		$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+		$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+		$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+		$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+		$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by name that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByName() {
+		// Grab a name that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, "not open :(", $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$this->assertNull($restaurant);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by address
+	 */
+	public function testGetValidRestaurantByAddress() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurant = Restaurant::getRestaurantByAddress($this->getPDO(), $restaurant->getAddress());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+		$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+		$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+		$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+		$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+		$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+		$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+		$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by address that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByAddress() {
+		// Grab a name that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, "123 GET THE F*** AWAY FROM ME ST.", $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$this->assertNull($restaurant);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by phone
+	 */
+	public function testGetValidRestaurantByPhone() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurant = Restaurant::getRestaurantByPhone($this->getPDO(), $restaurant->getPhone());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+		$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+		$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+		$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+		$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+		$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+		$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+		$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by phone that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByPhone() {
+		// Grab a name that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, "5058008135", $this->VALID_FORK_RATING);
+		$this->assertNull($restaurant);
+	}
+
+	/**
+	 * Test grabbing a Restaurant by fork rating
+	 */
+	public function testGetValidRestaurantByForkRating() {
+		// Count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("restaurant");
+
+		// Create a new Restaurant and insert it into MySQL
+		$restaurant = new Restaurant(null, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, $this->VALID_FORK_RATING);
+		$restaurant->insert($this->getPDO());
+
+		// Grab the data from MySQL and enforce the fields match out expectations
+		$pdoRestaurants = Restaurant::getRestaurantsByForkRating($this->getPDO(), $restaurant->getForkRating());
+		foreach($pdoRestaurants as $pdoRestaurant) {
+			$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("restaurant"));
+			$this->assertSame($pdoRestaurant->getRestaurantId(), $this->VALID_ID);
+			$this->assertSame($pdoRestaurant->getGoogleId(), $this->VALID_GOOGLE_ID);
+			$this->assertSame($pdoRestaurant->getFacilityKey(), $this->VALID_FACILITY_KEY);
+			$this->assertSame($pdoRestaurant->getName(), $this->VALID_NAME);
+			$this->assertSame($pdoRestaurant->getAddress(), $this->VALID_ADDRESS);
+			$this->assertSame($pdoRestaurant->getPhone(), $this->VALID_PHONE);
+			$this->assertSame($pdoRestaurant->getForkRating(), $this->VALID_FORK_RATING);
+		}
+	}
+
+	/**
+	 * Test grabbing a Restaurant by fork rating that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testGetInvalidRestaurantByForkRating() {
+		// Grab a name that does not exist
+		$restaurant = new Restaurant(TruForkTest::INVALID_KEY, $this->VALID_ID, $this->VALID_GOOGLE_ID, $this->VALID_FACILITY_KEY, $this->VALID_NAME, $this->VALID_ADDRESS, $this->VALID_PHONE, "u suck");
+		$this->assertNull($restaurant);
 	}
 
 }
