0a50f64db1dd26ea693d56812c7097ce63fa5404
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-14 10:53:00 -0600

    Updated header https://app.asana.com/0/42983347910415/45603236022887

--- public_html/php/lib/header.php
+++ public_html/php/lib/header.php
@@ -1,19 +1,19 @@
 <div class="container" xmlns="http://www.w3.org/1999/html">
 	<div class="row">
-		<div class="col-md-4">
-			<a class="home-link" href="#">
-				<span class="title">
-					<img class="logo" src="images/fork.svg" alt="TruFork Logo"/>
-					TruFork
+		<div class="col-md-3">
+			<a class="home-link" href=".">
+				<span class=" title">
+				<img class="logo" src="images/fork.svg" alt="TruFork Logo"/>
+				TruFork
 				</span>
 			</a>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<input class="search" type="search" placeholder="Search in Albuquerque"/>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-6">
 			<ul class="nav nav-pills pull-right">
-				<li role="presentation"><a href="#">Home</a></li>
+				<li role="presentation"><a href="#">Login/Register</a></li>
 				<li role="presentation"><a href="epic/epic.php">Epic</a></li>
 				<li role="presentation"><a href="epic/epic-addendum.php">Addendum</a></li>
 			</ul>
