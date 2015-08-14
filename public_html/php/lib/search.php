4e22bf5abbafdd310e555e92d92b9bb456ef3342
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-14 16:25:58 -0600

    Made search bars way better (except the top one there are problems) https://app.asana.com/0/42983347910415/43141204807877

--- public_html/php/lib/search.php
+++ public_html/php/lib/search.php
@@ -1,14 +1,21 @@
 <div class="main-text">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h1>Find a delicious, clean place to eat.</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12">
-				<input class="main-search" type="search" placeholder="Search in Albuquerque"/>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Find a delicious, clean place to eat.</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <form id="main-search" method="get" action="">
+                    <div class="col-xs-10 search-bar-left">
+                        <input class="main-search" type="search" placeholder="Search in Albuquerque"/>
+                    </div>
+                    <div class="col-xs-2 search-bar-right">
+                        <input class="main-search-button" type="submit" value="Search"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
