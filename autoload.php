0e4c0273ba9ad3ea0ed0330de5e6d1c8e935e560
TrevorRigler <tarigler@gmail.com>
2015-08-17 09:38:47 -0600

    edited autoloader so it only uses Skyler's code
    https://app.asana.com/0/41180108506050/45603234194182

--- autoload.php
+++ autoload.php
@@ -1,42 +1,7 @@
 <?php
 
 /**
- * Version 1 of Autoloader
- * using __autoload bc it's a magic method and I want to memorialize Doug Henning
- * Not sure it groks the pathname; trying to get it to instantiate all of the files in that directory
- * @param string $class name of class to load
- */
-function __autoload($class) {
-	require 'php/classes/' . $class . '.php';
-}
-
-/**
- * Version 2 of Autoloader
- * using spl_autoload_register bc php.net sez autoload may get deprecated
- * Made a named function bc trufork_autoloader sounds like a cool mid-80s industrial group
- * Doesn't seem to understand my syntax re path
- * Trying to get it to load all *.php files (sryNotSry for * tho)
- * @param string $class name of class to load
- *
- */
-function trufork_autoloader($class) {
-	require 'public_html/php/classes/' . $class . '.php';
-}
-
-spl_autoload_register('trufork_autoloader');
-
-/**
- * Version 3 of Autoloader
- * mostly same as v2
- * using generic anon function instead of named function so The Man can't track us
- * @param string $class name of class to load
- */
-spl_autoload_register(function ($class) {
-	require 'php/classes/' . $class . '.php';
-});
-
-/**
- * Version 4 of Autoloader
+ * Class Autoloader
  * stolen from Skyler
  * sin verguenza
  */
@@ -55,7 +20,6 @@ class Autoloader {
 		if(is_readable(__DIR__ . "/$className.php")) {
 			require_once(__DIR__ . "/$className.php");
 		} else {
-			echo "I can't even!";
 			throw(new Exception("Unable to load $className.php"));
 		}
 	}
