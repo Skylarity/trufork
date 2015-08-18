323ccb27192fb46c2914f54639a568bb9cf2c499
TrevorRigler <tarigler@gmail.com>
2015-08-18 15:26:09 -0600

    edited restaurant-comment-form.php, added controllers/restaurant-comment-controller.php, lib/aes256.php, lib/encrypted-config.php, and lib/xsrf.php https://app.asana.com/0/41180108506050/45606233086407

---
+++ public_html/php/controllers/restaurant-comment-controller.php
@@ -0,0 +1,26 @@
+<?php
+require_once(dirname(__DIR__) . "classes/comment.php");
+require_once(dirname(__DIR__) . "lib/xsrf.php");
+requireonce(dirname(__DIR__) . "lib/encrypted-config.php");
+
+try {
+	// ensure te field is actually filled out properly
+	if(@isset($_POST["commentContent"]) === false) {
+		throw(new InvalidArgumentException ("Comment not complete. Please try again."));
+	}
+
+	// verify the XSRF challenge
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	verifyXsrf();
+
+	// create the new comment and insert into mySQL
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
+	$comment = new Comment(null, $_POST["commentContent"]);
+	$comment->insert($pdo);
+	echo "<p class=\"alert alert-success\">Comment (id = " . $comment->getCommentId() . ") posted!</p>";
+}catch(Exception $exception) {
+	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+
+}
