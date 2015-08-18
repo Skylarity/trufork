d2c358712440d8d516ee9972062da056e8279926
TrevorRigler <tarigler@gmail.com>
2015-08-18 15:30:13 -0600

    typos in restaurant-comment-controller.php
    https://app.asana.com/0/41180108506050/45606233086407

--- public_html/php/controllers/restaurant-comment-controller.php
+++ public_html/php/controllers/restaurant-comment-controller.php
@@ -1,7 +1,8 @@
 <?php
 require_once(dirname(__DIR__) . "classes/comment.php");
+require_once(dirname(__DIR__) . "lib/restaurant.php");
 require_once(dirname(__DIR__) . "lib/xsrf.php");
-requireonce(dirname(__DIR__) . "lib/encrypted-config.php");
+require_once(dirname(__DIR__) . "lib/encrypted-config.php");
 
 try {
 	// ensure te field is actually filled out properly
@@ -22,5 +23,7 @@ try {
 	echo "<p class=\"alert alert-success\">Comment (id = " . $comment->getCommentId() . ") posted!</p>";
 }catch(Exception $exception) {
 	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+
+	//need to include sign-in info from session (see https://bootcamp-coders.cnm.edu/class-materials/php/sessions/)
 
 }
