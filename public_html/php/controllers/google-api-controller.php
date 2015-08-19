113a40a74126b12cab14b75b235e64d23fd6a056
Humberto Perez <ferdinero121312@gmail.com>
2015-08-19 13:48:11 -0700

    I input sanitized url https://app.asana.com/0/41180108506150/43141204807867

--- public_html/php/controllers/google-api-controller.php
+++ public_html/php/controllers/google-api-controller.php
@@ -1,22 +1,38 @@
-<!DOCTYPE html>
-<html>
-	<body>
+<?php
+require_once("/etc/apache2/data-design/encrypted-config.php");
 
-		<?php
-		$url = "https: //maps.googleapis.com/map/api/textsearch/json?.key=$config["AIzaSyDrzVsv2pExMzhxY-EkXNyaEmOtZkA8Qac"]&location = 35.08574,-106.64953. & radius=20000.&types=cafe|food|restaurant&query=$userquery";
+try {
+//grab api key from config file
+	$config = readConfig("/etc/apache2/capstone-mysql/trufork.ini");
 
-		// Remove all illegal characters from a url
-		$url = filter_var($url, FILTER_SANITIZE_URL);
+//grab user query, sanitize and validate
+	$userQuery = filter_input(INPUT_GET, "userQuery", FILTER_SANITIZE_STRING);
+	$userQuery = trim($userQuery);
+	if(empty($userQuery) === true) {
+		throw(new InvalidArgumentException("user query is empty or insecure"));
+	} else {
+		$userQuery = urlencode($userQuery);
+	}
 
-		// Validate url
-		if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
-			echo("$url is a valid URL");
-		} else {
-			echo("$url is not a valid URL");
-		}
-		?>
-
-	</body>
-</html>
+//construct url
+	$url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" . $config["placekey"] . "&location=35.08574,-106.64953&radius=20000&types=cafe|food|restaurant&query=" . $userQuery;
 
 
+	$response = file_get_contents( $url);
+			if ($response->status === "ok") {
+				var_dump(json_decode($response));
+
+			} else  if($response-> status ==="zero_results"){
+							throw(newInvalitArgumentException("this search get zero resul nada"));
+			}
+} catch(InvalidArgumentException $invalidArgument) {
+	throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+} catch(RangeException $range) {
+	// Rethrow exception to the caller
+	throw(new RangeException($range->getMessage(), 0, $range));
+} catch(Exception $exception) {
+	// HUMBERTO WILL WRITE EXCEPTION HANDLING HERE :D
+	// Rethrow exception to the caller
+	throw(new Exception($exception->getMessage(), 0, $exception));
+}
+?>
