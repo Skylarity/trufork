e627fc8feebb0904e46d7769d81f3d4c2d1ad4e7
Humberto Perez <ferdinero121312@gmail.com>
2015-08-19 15:40:45 -0700

    I input sanitized url https://app.asana.com/0/41180108506150/43141204807867

--- public_html/php/controllers/google-api-controller.php
+++ public_html/php/controllers/google-api-controller.php
@@ -2,10 +2,10 @@
 require_once("/etc/apache2/data-design/encrypted-config.php");
 
 try {
-//grab api key from config file
+	//grab api key from config file
 	$config = readConfig("/etc/apache2/capstone-mysql/trufork.ini");
 
-//grab user query, sanitize and validate
+	//grab user query, sanitize and validate
 	$userQuery = filter_input(INPUT_GET, "userQuery", FILTER_SANITIZE_STRING);
 	$userQuery = trim($userQuery);
 	if(empty($userQuery) === true) {
@@ -14,17 +14,27 @@ try {
 		$userQuery = urlencode($userQuery);
 	}
 
-//construct url
+	//construct url
 	$url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" . $config["placekey"] . "&location=35.08574,-106.64953&radius=20000&types=cafe|food|restaurant&query=" . $userQuery;
 
+	$response = file_get_contents($url);
+	$data = json_decode($response);
+	var_dump($data);
 
-	$response = file_get_contents( $url);
-			if ($response->status === "ok") {
-				var_dump(json_decode($response));
+	if($data->status === "OK"){
+		foreach($data->results as $result) {
+			echo "<ul class=\"list-group\">" . PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["name"] . "</strong></li>" . PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["rating"]. "</strong></li>". PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["photos"]. "</strong></li>". PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["place_id"]."</strong></li>". PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["geometry"]."</strong></li>". PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result["formatted_address"]. "</strong></li>". PHP_EOL;
+		}
+	} else {
+		echo "error message here";
+	}
 
-			} else  if($response-> status ==="zero_results"){
-							throw(newInvalidArgumentException("this search get zero resul nada"));
-			}
 } catch(InvalidArgumentException $invalidArgument) {
 	throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
 } catch(RangeException $range) {
@@ -35,4 +45,3 @@ try {
 	// Rethrow exception to the caller
 	throw(new Exception($exception->getMessage(), 0, $exception));
 }
-?>
