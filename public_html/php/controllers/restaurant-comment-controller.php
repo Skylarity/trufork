5a62e2ab1c361419be1eac95573f21355741fe02
TrevorRigler <tarigler@gmail.com>
2015-08-20 08:50:20 -0600

    trying to fix issue so form will pass dateTime
    https://app.asana.com/0/41180108506050/45606233086407

--- public_html/php/controllers/restaurant-comment-controller.php
+++ public_html/php/controllers/restaurant-comment-controller.php
@@ -18,10 +18,13 @@ try {
 
 	// create the new comment and insert into mySQL
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
-	$comment = new Comment(null, $_POST["profileId"], $_POST["restaurantId"], $_POST["dateTime"], $_POST["content"]);
+	$newDateTime = new dateTime("now");
+	//echo($newDateTime->format('Y-m-d H:i:s'));
+	$comment = new Comment(null, $_POST["profileId"], $_POST["restaurantId"], $_POST["dateTime"], $_POST["txtComment"]);
+	//$comment = new Comment(null, null, null, null, null);
 	$comment->insert($pdo);
 	echo "<p class=\"alert alert-success\">Comment (id = " . $comment->getCommentId() . ") posted!</p>";
-}catch(Exception $exception) {
+} catch(Exception $exception) {
 	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
 
 	//need to include sign-in info from session (see https://bootcamp-coders.cnm.edu/class-materials/php/sessions/)
