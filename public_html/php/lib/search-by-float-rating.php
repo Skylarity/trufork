4973e5cfc9ebeb21ef7ec7eb2fb47f831bb33527
TrevorRigler <tarigler@gmail.com>
2015-08-21 09:41:50 -0600

    adding very preliminary frontend mockup for 'search by trufork rating' function
    https://app.asana.com/0/41180108506050/45604655562203

---
+++ public_html/php/lib/search-by-float-rating.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+
+<?php
+require_once(dirname(__DIR__) . "/classes/restaurant.php");
+require_once(dirname(__DIR__) . "/lib/xsrf.php");
+require_once("/etc/apache2/data-design/encrypted-config.php");
+?>
+
+<div class="um" id="search-by-trufork-rating">
+	<h2>Search by TruFork Rating</h2>
+	<!-- <div class="content">
+		<div class="main-container rating-widget rating-widget">
+
+		</div>
+	</div> -->
+</div>
+<form action="#" method="POST">
+	<div class="review-stars clearfix">
+		<fieldset class="star-rating-widget inline-block">
+			<legend class="offscreen">Rating</legend>
+			<ul class="stars-0">
+				<li>
+					<input id="rating-5" name="rating" type="radio" value="5">
+					<label for="rating-5">5 (Highest reviews and impeccable inspection records.)</label>
+				</li>
+				<li>
+					<input id="rating-4" name="rating" type="radio" value="1">
+					<label for="rating-4">4 (Good reviews and inspection records.)</label>
+				</li>
+				<li>
+					<input id="rating-3" name="rating" type="radio" value="2">
+					<label for="rating-3">3 (Caution. Reviews and inspection records may indicate trouble.)</label>
+				</li>
+				<li>
+					<input id="rating-2" name="rating" type="radio" value="3">
+					<label for="rating-2">2 (I too like to live dangerously.)</label>
+				</li>
+				<li>
+					<input id="rating-1" name="rating" type="radio" value="4">
+					<label for="rating-1">4 (Avoid like the plague.)</label>
+				</li>
+			</ul>
+			<p class="description">Roll over stars, choose a rating, then click Search.</p>
