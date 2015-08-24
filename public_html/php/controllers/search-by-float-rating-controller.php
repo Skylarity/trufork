7b11858d291d94a3d8d83c11873ee0429347badc
TrevorRigler <tarigler@gmail.com>
2015-08-24 14:55:12 -0600

    more non-substantive changes to *.js and *.php parts of MVC for search by rating
    https://app.asana.com/0/41180108506050/45604655562203

--- public_html/php/controllers/search-by-float-rating-controller.php
+++ public_html/php/controllers/search-by-float-rating-controller.php
@@ -2,16 +2,18 @@
 
 /** Search by TruFork Rating Controller
  * This is part of an MVC
- * Searches restaurant class/table
+ * Searches restaurant class/table by trufork rating
  */
 
 require_once(dirname(__DIR__) . "/classes/restaurant.php");
+require_once(dirname(__DIR__) . "/lib/xsrf.php");
 require_once("/etc/apache2/data-design/encrypted-config.php");
 
-$mysqli = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
+$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
 
-/** select queries and return a result set */
-if ($result = $mysqli->query("SELECT name FROM restaurant WHERE forkRating >= 3")) {
+/** select queries and return a result set
+ 	 currently only permits db search of values btwn 3.0 and 3.9 */
+if ($result = $mysqli->query("SELECT name FROM restaurant WHERE forkRating >= 3 < 4")) {
 	printf("Select returned %d rows.\n", $result->num_rows);
 
 	/** free result set */
@@ -19,3 +21,4 @@ if ($result = $mysqli->query("SELECT name FROM restaurant WHERE forkRating >= 3"
 }
 
 $mysqli->close();
+
--- public_html/php/controllers/search-by-float-rating-form.php
+++ public_html/php/controllers/search-by-float-rating-form.php
@@ -1,41 +1,43 @@
 <form id="search-by-float-rating-form" action="<?php echo $PREFIX; ?>php/controllers/search-by-float-rating-controller.php" method="post">
 
-<?php
-require_once(dirname(__DIR__) . "/classes/restaurant.php");
-require_once("/etc/apache2/data-design/encrypted-config.php");
-?>
-
-<div class="um" id="search-by-trufork-rating">
+<div class="search-by-float-rating-form-group">
 	<h2>Search by TruFork Rating</h2>
-</div>
 
 <form action="#" method="POST">
 	<div class="review-stars clearfix">
-		<fieldset class="star-rating-widget inline-block">
-			<legend class="offscreen">Rating</legend>
+
+		<fieldset class="box-rating-widget inline-block">
+			<legend class="offscreen">TruFork Rating</legend>
 			<ul class="stars-0">
 				<li>
-					<input id="rating-5" name="rating" type="radio" value="5">
-					<label for="rating-5">5 (Highest reviews and impeccable inspection records. Buen provecho!)</label>
+					<input id="rating-5" name="rating" type="checkbox" value="5">
+					<label for="rating-5">5: Highest reviews and impeccable inspection records. Buen provecho!</label>
 				</li>
 				<li>
-					<input id="rating-4-4.9" name="rating" type="radio" value="1">
-					<label for="rating-4-4.9">4 (Good reviews and inspection records. Proceed.)</label>
+					<input id="rating-4-4.9" name="rating" type="checkbox" value="4-4.9">
+					<label for="rating-4-4.9">4 - 4.9: Good reviews and inspection records. Proceed.</label>
 				</li>
 				<li>
-					<input id="rating-3-3.9" name="rating" type="radio" value="2">
-					<label for="rating-3-3.9">3 (Caution. Reviews and inspection records may indicate trouble.)</label>
+					<input id="rating-3-3.9" name="rating" type="checkbox" value="3-3.9">
+					<label for="rating-3-3.9">3 - 3.9: Caution. Reviews and inspection records may indicate trouble.</label>
 				</li>
 				<li>
-					<input id="rating-2-2.9" name="rating" type="radio" value="3">
-					<label for="rating-2-2.9">2 (I too like to live dangerously.)</label>
+					<input id="rating-2-2.9" name="rating" type="checkbox" value="2-2.9">
+					<label for="rating-2-2.9">2 - 2.9: I too like to live dangerously.</label>
 				</li>
 				<li>
-					<input id="rating-1-1.9" name="rating" type="radio" value="4">
-					<label for="rating-1-1.9">1 (Unless this place is new and has no reviews, avoid like the plague.)</label>
+					<input id="rating-1-1.9" name="rating" type="checkbox" value="1-1.9">
+					<label for="rating-1-1.9">1 - 1.9: Unless this place is new and has no reviews, avoid like the plague.</label>
 				</li>
 			</ul>
-			<p class="description">Roll over stars, choose a rating, then click Search.</p>
+			<p class="description">Choose one or more rating levels, then click Submit.</p>
+
+			<div class="button-group">
+				<button type="submit" class="btn btn-primary">Submit</button>
+				<button type="reset" class="btn btn-default">Reset</button>
+			</div>
+	</div>
+
 </form>
 			<div id="outputArea"></div>
 
