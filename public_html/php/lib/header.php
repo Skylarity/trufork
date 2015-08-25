a01bae085a708c06be1ab44d9b2c161809c5a701
Kenneth Chavez <kchavez.bills@gmail.com>
2015-08-25 09:54:39 -0600

    sign-up refactor  https://app.asana.com/0/41180108506094/45605980210388

--- public_html/php/lib/header.php
+++ public_html/php/lib/header.php
@@ -22,10 +22,9 @@
 			<ul class="nav nav-pills pull-right">
 
 				<!--Button Trigger User Login--> 
-				<button type="button" class="modal-button" data-toggle="modal" data-target="#myModal"> 
-					Sign Up
-				</button>
-				 
+				<li><button type="button" class="modal-button" data-toggle="modal" data-target="#myModal">Sign Up</button></li>
+				<li><button type="button" class="modal-button" data-toggle="modal" data-target="#loginModal"> Login</button></li>
+
 				<!-- 	login modal--> 
 				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 					<div class="modal-dialog"> 
@@ -75,55 +74,62 @@
 									</div>
 							</div>
 							<div id="outputArea"></div>
-							<script type="text/javascript" src="<?php echo $PREFIX; ?>php/controllers/signup-controller.js"></script>
+							<script type="text/javascript" src="<?php echo $PREFIX; ?>php/controllers/sign-up-controller.js"></script>
 						</form>
+
+						<!-- 	login modal--> 
+						<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							<div class="modal-dialog"> 
+								<form id="signUpForm" name="signUpForm" class="form" action="<?php echo $PREFIX?>php/controllers/sign-up-controller.php" method="post">
+
+									<div class="modal-content">
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+												aria-hidden="true">&times;</span>
+										</button>
+										<div class="modal-header">
+											<h3>Create A New Account</h3>
+										</div>
+
+										<div class="form-group">
+											<label for="userName">User Name</label>
+											<input type="text" class="form-control" id="userName" name="userName" placeholder="choose a username">
+										</div>
+
+										<div class="form-group">
+											<label for=password>Password</label>
+											<input type="password" class="form-control" id="password" name="password" placeholder="password">
+										</div>
+
+										<div class="form-group">
+											<label for="verifyPassword">Verify Password</label>
+											<input type="password" class="form-control" id="verifyPassword" name="verifyPassword" placeholder="verify password">
+										</div>
+
+										<div class="form-group">
+											<div class="checkbox">
+												<label>
+													<input type="checkbox" id="rememberMe[]" name="rememberMe" value="Remember Me"> Remember Me
+												</label>
+											</div>
+										</div>
+
+										<div class="form-group">
+											<div class="modal-footer"
+											<label>
+												<button type="submit" class="modal-button" id="submitButton" name="submitButton">Create Account</button>
+											</label>
+										</div>
+									</div>
+									<div id="outputArea"></div>
+									<script type="text/javascript" src="<?php echo $PREFIX; ?>php/controllers/sign-up-controller.js"></script>
+								</form>
 
 
 
 					</div>
 				</div>
 
-<!--				<!--Button Trigger User Login--> -->
-<!--				<button type="button" class="modal-button" data-toggle="modal" data-target="#myModal"> -->
-<!--					Log In-->
-<!--				</button>-->
-<!--				 -->
-<!--				<!-- 	login modal--> -->
-<!--				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">-->
-<!--					<div class="modal-dialog"> -->
-<!--						<form id="profileController" class="form" method="post" action="../../php/controllers/profile-controller.php">-->
-<!--							<div class="modal-content">-->
-<!--								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span-->
-<!--										aria-hidden="true">&times;</span>-->
-<!--								</button>-->
-<!--								<div class="modal-header">-->
-<!--									<h3>Log In</h3>-->
-<!--								</div>-->
-<!---->
-<!--								<div class="form-group">-->
-<!--									<label for="UserId">User Name</label>-->
-<!--									<input type="UserId" class="form-control" id="UserId" placeholder="Choose a username">-->
-<!--								</div>-->
-<!---->
-<!--								<div class="form-group">-->
-<!--									<label for="hash1">Password</label>-->
-<!--									<input type="password" class="form-control" id="verifyPassword1" placeholder="password">-->
-<!--								</div>-->
-<!---->
-<!--								<div class="form-group">-->
-<!--									<div class="modal-footer"-->
-<!--									<label>-->
-<!--										<button type="submit" class="modal-button">Create Account</button>-->
-<!--									</label>-->
-<!--								</div>-->
-<!--							</div>-->
-<!--						</form>-->
-<!--					</div>-->
-<!--				</div>-->
 
-				<!--				<li role="presentation"><a href="#">Login/Register</a></li>-->
-				<!--				-<li role="presentation"><a href="epic/epic.php">Epic</a></li>-->
-				<!--				<li role="presentation"><a href="epic/epic-addendum.php">Addendum</a></li>-->
 
 			</ul>
 		</div>
