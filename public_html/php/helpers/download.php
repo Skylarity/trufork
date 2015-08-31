5854fc658d7e4a48059954570d5dd469c9d2269c
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-31 15:22:18 -0600

    Moved the actual downloading part to a separate file https://app.asana.com/0/42983347910415/43141204807877

--- public_html/php/helpers/download.php
+++ public_html/php/helpers/download.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("../classes/data-downloader.php");
+require_once(dirname(__DIR__) . "/classes/data-downloader.php");
 
 // This downloads the file to the bootcamp server
 DataDownloader::downloadIfNew("http://data.cabq.gov/business/LIVES/businesses.csv", "/var/lib/abq-data/", "businesses", ".csv");
