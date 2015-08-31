1e8ba81073a1df1d0d17b9a11f04f9f9935d78ee
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-31 14:13:42 -0600

    Moved the actual downloading part to a separate file https://app.asana.com/0/42983347910415/43141204807877

--- public_html/php/classes/data-downloader.php
+++ public_html/php/classes/data-downloader.php
@@ -397,9 +397,3 @@ class DataDownloader {
 	}
 
 }
-
-// This downloads the file to the bootcamp server
-DataDownloader::downloadIfNew("http://data.cabq.gov/business/LIVES/businesses.csv", "/var/lib/abq-data/", "businesses", ".csv");
-DataDownloader::downloadIfNew("http://data.cabq.gov/business/LIVES/violations.csv", "/var/lib/abq-data/", "violations", ".csv");
-// This fills the database with information
-DataDownloader::fillDatabase("/var/lib/abq-data/businesses", ".csv", "/var/lib/abq-data/violations", ".csv");
---
+++ public_html/php/helpers/download.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("../classes/data-downloader.php");
+
+// This downloads the file to the bootcamp server
+DataDownloader::downloadIfNew("http://data.cabq.gov/business/LIVES/businesses.csv", "/var/lib/abq-data/", "businesses", ".csv");
+DataDownloader::downloadIfNew("http://data.cabq.gov/business/LIVES/violations.csv", "/var/lib/abq-data/", "violations", ".csv");
+// This fills the database with information
+DataDownloader::fillDatabase("/var/lib/abq-data/businesses", ".csv", "/var/lib/abq-data/violations", ".csv");
