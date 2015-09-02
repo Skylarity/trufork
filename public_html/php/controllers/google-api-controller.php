b7c7d79800182b3d61456c0033ae66938c249e21
hperezperez <hperezperez@cnm.edu>
2015-09-02 13:18:08 -0600

    semi clean version of google-api-controller.php

--- public_html/php/controllers/google-api-controller.php
+++ public_html/php/controllers/google-api-controller.php
@@ -2,13 +2,14 @@
 
 require_once(dirname(__DIR__) . "/classes/data-downloader.php");
 require_once(dirname(__DIR__) . "/classes/restaurant.php");
-
+require_once(dirname(__DIR__) . "/classes/violation.php");
 
 require_once("/etc/apache2/data-design/encrypted-config.php");
 
 try {
 	//grab api key from config file
 	$config = readConfig("/etc/apache2/capstone-mysql/trufork.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
 
 	//grab user query, sanitize and validate
 	$userQuery = filter_input(INPUT_GET, "userQuery", FILTER_SANITIZE_STRING);
@@ -18,141 +19,89 @@ try {
 	} else {
 		$userQuery = urlencode($userQuery);
 	}
-
 	//construct url
 	$url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" . $config["placekey"] . "&location=35.08574,-106.64953&radius=20000&types=cafe|food|restaurant&query=" . $userQuery;
 
 	$response = file_get_contents($url);
 	$data = json_decode($response);
-	var_dump($data);
 
 	if($data->status === "OK") {
 		foreach($data->results as $result) {
-			$photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxheight=512&photoreference=" . $result->photos[0]->photo_reference . "&key=" . $config["placekey"];
+			$rating = 0.0;
+			if(empty($result->rating) === false) {
+				$rating = $result->rating;
+			}
+
 			$imgUrl = null;
-			$metadata = DataDownloader::getMetaData($photoUrl, 0);
-			foreach($metadata["wrapper_data"] as $header) {
-				if(strpos($header, "Location: ") === 0) {
-					$imgUrl = substr($header, 10);
-					break;
+			if(empty($result->photos) === false) {
+				$photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxheight=512&photoreference=" . $result->photos[0]->photo_reference . "&key=" . $config["placekey"];
+				$metadata = DataDownloader::getMetaData($photoUrl, 0);
+				foreach($metadata["wrapper_data"] as $header) {
+					if(strpos($header, "Location: ") === 0) {
+						$imgUrl = substr($header, 10);
+						break;
+					}
 				}
 			}
-			//write the google place information that we need
 			if($imgUrl !== null) {
 				echo "<img class=\"img-responsive\" src=\"$imgUrl\" />" . PHP_EOL;
 			}
 			echo "<ul class=\"list-group\">" . PHP_EOL;
 			echo "<li class=\"list-group-item\"><strong>" . $result->formatted_address . "</strong></li>" . PHP_EOL;
-							echo "<li class=\"list-group-item\"><strong>" . $result->name . "</strong></li>" . PHP_EOL;
-			echo "<li class=\"list-group-item\"><strong>" . $result->rating . "</strong></li>" . PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $result->name . "</strong></li>" . PHP_EOL;
+			echo "<li class=\"list-group-item\"><strong>" . $rating . "</strong></li>" . PHP_EOL;
 			echo "<li class=\"list-group-item\"><strong>" . $result->place_id . "</strong></li>" . PHP_EOL;
-			//	echo "<li class=\"list-group-item\"><strong>" . $result->price_level . "</strong></li>" . PHP_EOL;
 			echo "<li class=\"list-group-item\"><strong>" . $result->geometry->location->lat . "</strong></li>" . PHP_EOL;
 			echo "<li class=\"list-group-item\"><strong>" . $result->geometry->location->lng . "</strong></li>" . PHP_EOL;
 			echo "</ul>";
 
-
-			$detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?key=" . $config["placekey"]. "&placeid=".
+			$detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?key=" . $config["placekey"] . "&placeid=" .
 				$result->place_id;
-				$detailsResponse = json_decode(file_get_contents($detailsUrl));
-				var_dump($detailsResponse->result);
+	    		$detailsResponse = json_decode(file_get_contents($detailsUrl));
 
-				if($detailsResponse->status ==="OK") {
-//							foreach($detailsResponse->result as $response=>$response_value){
-//											echo "direccion y tel".$response."y aqui q".$response_value;
-//							}
-					//		}}
-					$fullAddress = [];
-					$gooArray = array();
-					$address_components = $detailsResponse->address_components;
-
-					//foreach($address_components as $component) {
-					foreach($address_components as $component) {
-						echo "dir=" . $component;
-						$fullAddress[] = $component["short_name"];
+			 if($detailsResponse->status === "OK") {
+				$fullAddress = ["street_number" => null, "route" => null];
+				$gooArray = array();
+				$address_components = $detailsResponse->result->address_components;
+				foreach($address_components as $component) {
+					foreach($component->types as $type) {
+						if($type === "route") {
+							$fullAddress["route"] = $component->short_name;
+						}
+						if($type === "street_number") {
+							$fullAddress["street_number"] = $component->short_name;
+						}
 					}
-
-					$fullAddress = implode(" ", $fullAddress);
-					echo "$fullAddress";
 				}
+				$fullAddress = implode(" ", $fullAddress);
+			}
 			// we will put ours address in capital letters
+		   	$fullAddress = strtoupper($fullAddress);
 
-			$fullAddress = strtoupper($fullAddress);
-
-
-			// this is one of the solution that the master Dylan suggest
-			$detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?key=". $config["placekey"] . "&placeid=" . $result->place_id;
-			$detailsResponse = json_decode(file_get_contents($detailsUrl));
-			var_dump($detailsResponse->result);
-			if($detailsResponse->status === "OK"){
-				foreach($detailsResponse->results as $response) {
-// create map to secondary associative array here
-					$shortNumber = $response->address_components[0]->short_name;
-					$shortAddress = $response->address_components[1]->short_name;
-					echo "esto es{$shortNumber}!";
-				}
-			}
-
-			echo "<ul class=\list-group\">". PHP_EOL;
-					echo("la dir formateada");
-			echo "<li class=\list-group-item\><strong>". $response->$shortNumber . "</strong></li>". PHP_EOL;
-			echo "<li class=\list-group-item\><strong>". $response->$shortAddress ."</strong></li>". PHP_EOL;
-
-			/*
-					*  this subcode is trying to match the city address  with google address
-			*/
-
-					function myfunction($g,$c){
-						if($g==$c){
-							return" same";
-						} return "diferent";
-					}
-
-			$g= array("$shortNumber","$shortAddress");
-			$c= array("4717"," old coors nw");
-			print_r(array_map("myfunction",$g,$c));
-			$restaurant = array();
-			$gMatches = array();
-			$cMatches = array();
-			$pattern = "/^(\d{1,6)\s+(.+)\s+(NE|NW|SE|SW).*(\d+)?$/iU";
-			// how we grab the address from goggle
-
-			$subjectg = $result->formatted_address;
-
-		//	$subjectg= $result->formatted_address;
-		//	$subjectg=$result->address_components;
-			$subjectg=$result-> formatedd_address;
-			echo strtoupper("$subjectg");
-			echo ("aqui es");
-			var_dump($subjectg);
-			$googleId = $result->place_id;
-
-
-			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
-			$restaurant = Restaurant::getRestaurantByAddress($pdo, $subjectg);
-			var_dump($restaurant);
-			if($restaurant !== null) {
-				$subjectc = $restaurant->getAddress();
-				preg_match($pattern, $subjectg, $gMatches);
-				preg_match($pattern, $subjectc, $cMatches);
-
-				$g0 = $gMatches[0];
-				$c0 = $cMatches[0];
-				if($g0 === $c0) {
-					//create a new Restaurant object, and output it to an array ($restaurants)
-					$restaurant = Restaurant::getRestaurantByGoogleId($pdo, $googleId);
-					if($restaurant === null) {
-						$restaurant = Restaurant::getRestaurantByAddress($pdo, $g0);
-						$restaurant->setGoogleId($googleId);
-						$restaurant->update($pdo);
-						echo $restaurant;
+			  $googleId = $result->place_id;
+			//grap the address from city
+			      $matchedRestaurants = Restaurant::getRestaurantsByAddress($pdo, $fullAddress);
+			      foreach($matchedRestaurants as $matchedRestaurant) {
+				 $comments = 0;
+				$violations = Violation::getViolationByRestaurantId($pdo, $matchedRestaurant->getRestaurantId());
+				foreach($violations as $violation) {
+					if($violation->getViolationDesc() !== null) {
+						$comments++;
 					}
 				}
+				//creating the trufork rating
+				$truFork = $rating * (-0.125 * sqrt($comments) + 1);
+				$matchedRestaurant->setForkRating($truFork);
+				$matchedRestaurant->setGoogleId($result->place_id);
+				$matchedRestaurant->update($pdo);
 			}
 
+			//start the session
+			if(session_status() !== PHP_SESSION_ACTIVE) {
+				session_start();
+			}
+			$_SESSION["matchedRestaurants"] = $matchedRestaurants;
 		}
-	} else {
-		echo "error message here";
 	}
 
 } catch(InvalidArgumentException $invalidArgument) {
@@ -161,7 +110,6 @@ try {
 	// Rethrow exception to the caller
 	throw(new RangeException($range->getMessage(), 0, $range));
 } catch(Exception $exception) {
-	// HUMBERTO WILL WRITE EXCEPTION HANDLING HERE :D
 	// Rethrow exception to the caller
 	throw(new Exception($exception->getMessage(), 0, $exception));
 }
