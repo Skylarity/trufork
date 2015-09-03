661f226cc149ef301cc81e494641266681be2239
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-03 16:19:24 -0600

    Fixed header? https://app.asana.com/0/42983347910415/45603236022887

--- public_html/php/lib/header.php
+++ public_html/php/lib/header.php
@@ -1,25 +1,26 @@
 <div class="container">
-		<div class="row">
-			<div class="col-md-3">
-				<a class="home-link" href=".">
+	<div class="row">
+		<div class="col-md-3">
+			<a class="home-link" href=".">
 					<span class=" title">
-						<img class="logo" src="<?php echo $PREFIX; ?>images/logo" alt="TruFork Logo"/>
+						<img class="logo" src="<?php echo $PREFIX; ?>images/trufork-logo/tr-Icons/trufork-md.svg"
+							 alt="TruFork Logo"/>
 						TruFork
 					</span>
-				</a>
-			</div>
-			<div class="col-md-5">
-				<form class="form-inline" id="header-search" method="get" action="">
-					<div class="input-group">
-						<input class="header-search form-control" type="search" placeholder="Search in Albuquerque"/>
+			</a>
+		</div>
+		<div class="col-md-5">
+			<form class="form-inline" id="header-search" method="get" action="">
+				<div class="input-group">
+					<input class="header-search form-control" type="search" placeholder="Search in Albuquerque"/>
 							<span class="input-group-btn">
 								<button class="btn header-search-button" type="submit">Search</button>
 							</span>
-					</div>
-				</form>
-			</div>
-			<div class="col-md-4">
-				<?php require_once("header-buttons.php"); ?>
-			</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-4">
+			<?php require_once("header-buttons.php"); ?>
 		</div>
 	</div>
+</div>
