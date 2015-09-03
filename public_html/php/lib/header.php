3faf1357d157efeb646854b455981d31fc898304
Kenneth Chavez <kchavez.bills@gmail.com>
2015-09-03 15:59:19 -0600

    change logo all logo names to reflect lower case.
      https://app.asana.com/0/41180108506094/45605980210388

--- public_html/php/lib/header.php
+++ public_html/php/lib/header.php
@@ -1,26 +1,25 @@
 <div class="container">
-	<div class="row">
-		<div class="col-md-3">
-			<a class="home-link" href=".">
-					<span class="title">
-						<img class="logo" src="<?php echo $PREFIX; ?>images/trufork-logo/tf-icons/trufork-32px.svg"
-							 alt="TruFork Logo"/>
+		<div class="row">
+			<div class="col-md-3">
+				<a class="home-link" href=".">
+					<span class=" title">
+						<img class="logo" src="<?php echo $PREFIX; ?>images/logo" alt="TruFork Logo"/>
 						TruFork
 					</span>
-			</a>
-		</div>
-		<div class="col-md-5">
-			<form class="form-inline" id="header-search" method="get" action="">
-				<div class="input-group">
-					<input class="header-search form-control" type="search" placeholder="Search in Albuquerque"/>
+				</a>
+			</div>
+			<div class="col-md-5">
+				<form class="form-inline" id="header-search" method="get" action="">
+					<div class="input-group">
+						<input class="header-search form-control" type="search" placeholder="Search in Albuquerque"/>
 							<span class="input-group-btn">
 								<button class="btn header-search-button" type="submit">Search</button>
 							</span>
-				</div>
-			</form>
-		</div>
-		<div class="col-md-4">
-			<?php require_once("header-buttons.php"); ?>
+					</div>
+				</form>
+			</div>
+			<div class="col-md-4">
+				<?php require_once("header-buttons.php"); ?>
+			</div>
 		</div>
 	</div>
-</div>
