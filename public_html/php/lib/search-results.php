ce60e960dbb797ce5bc6982d88e72ab236aea6c4
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-03 16:39:01 -0600

    Worked on search results page https://app.asana.com/0/42983347910415/45603236022887

--- public_html/php/lib/search-results.php
+++ public_html/php/lib/search-results.php
@@ -1,14 +1,20 @@
-<div class="container">
-	<div class="row">
+<div class="results">
+	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<h2>Restaurant Name</h2>
+			<div class="result">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="result-restaurant-name">Restaurant Name</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<p class="result-restaurant-desc">
+							Restaurant description.
+						</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<p>
-				Restaurant description.
-			</p>
 		</div>
 	</div>
 </div>
