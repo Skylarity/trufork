741a1b9e1e693f8b70511fa9be32aec300a7336f
Kenneth Chavez <kchavez.bills@gmail.com>
2015-09-04 11:50:01 -0600

    Logout button takes you to same page
      https://app.asana.com/0/41180108506094/45605980210388

--- public_html/php/controllers/log-out-controller.php
+++ public_html/php/controllers/log-out-controller.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
 unset($_SESSION["user"]);
 //header("Refresh:0");
 //echo "foo!";
-header("Location:/~kchavez68/trufork/public_html");
-
-
+header("Location:index.php");
