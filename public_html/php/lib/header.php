474d060fb39c4164e93a4b381619a6e25ce2adae
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-04 12:06:59 -0600

    Fixed top search https://app.asana.com/0/42983347910415/45603236022887

--- public_html/php/lib/header.php
+++ public_html/php/lib/header.php
@@ -7,18 +7,21 @@
 			<a class="home-link" href="<?php echo $PREFIX; ?>">
 					<span class=" title">
 						<img class="logo" src="<?php echo $PREFIX; ?>images/trufork-logo/tr-icons/trufork-lg.svg"
-							  alt="TruFork Logo"/>
+							 alt="TruFork Logo"/>
 						TruFork
 					</span>
 			</a>
 		</div>
 		<div class="col-md-5">
-			<form class="form-inline" id="header-search" method="get" action="">
+			<form class="form-inline" id="main-search" method="get"
+				  action="<?php echo $PREFIX ?>php/controllers/google-api-controller.php">
 				<div class="input-group">
-					<input class="header-search form-control" type="search" placeholder="Search in Albuquerque"/>
-							<span class="input-group-btn">
-								<button class="btn header-search-button" type="submit">Search</button>
-							</span>
+					<input id="userQuery" name="userQuery" type="text" class="form-control main-search input-lg"
+						   placeholder="Search in Albuquerque">
+						<span class="input-group-btn">
+							<button id="restaurant-search-submit" type="submit" class="btn btn-lg main-search-button">
+								<i class="fa fa-search"></i></button>
+						</span>
 				</div>
 			</form>
 		</div>
