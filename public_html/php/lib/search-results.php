adf348ea8e0e1e3940d2ef3615441f8a86cd037b
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-04 08:46:03 -0600

    Worked on search results page https://app.asana.com/0/42983347910415/45603236022887

--- public_html/php/lib/search-results.php
+++ public_html/php/lib/search-results.php
@@ -1,20 +1,19 @@
 <div class="results">
 	<div class="container">
 		<div class="row">
-			<div class="result">
-				<div class="row">
-					<div class="col-md-12">
-						<h2 class="result-restaurant-name">Restaurant Name</h2>
+			<a href="#">
+				<div class="result">
+					<div class="row">
+						<div class="col-md-2">
+							<img class="img-responsive center-block"
+								 src="<?php echo $PREFIX; ?>images/trufork-logo/tr-Icons/trufork-lg.svg"/>
+						</div>
+						<div class="col-md-10">
+							<h2 class="result-restaurant-name">Restaurant Name</h2>
+						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<p class="result-restaurant-desc">
-							Restaurant description.
-						</p>
-					</div>
-				</div>
-			</div>
+			</a>
 		</div>
 	</div>
 </div>
