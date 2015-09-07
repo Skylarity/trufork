02b16fadbddcc7109991a1da260f2337891566a9
TrevorRigler <tarigler@gmail.com>
2015-09-07 12:42:30 -0600

    trufork star rating search is now functional
    https://app.asana.com/0/42983347910415/45604655562203

--- public_html/php/lib/search-main.php
+++ public_html/php/lib/search-main.php
@@ -30,17 +30,24 @@
 				<div class="trufork-search-align">
 					<div class="box-rating">
 						<fieldset class="rating">
-							<input class="star star-5" id="star-5" type="radio" name="star"/>
+							<input class="star star-5" id="star-5" name="rating[]" type="radio" value="5"/>
 							<label class="star star-5" for="star-5"></label>
-							<input class="star star-4" id="star-4" type="radio" name="star"/>
+							<input class="star star-4" id="star-4" name="rating[]" type="radio" value="4"/>
 							<label class="star star-4" for="star-4"></label>
-							<input class="star star-3" id="star-3" type="radio" name="star"/>
+							<input class="star star-3" id="star-3" name="rating[]" type="radio" value="3"/>
 							<label class="star star-3" for="star-3"></label>
-							<input class="star star-2" id="star-2" type="radio" name="star"/>
+							<input class="star star-2" id="star-2" name="rating[]" type="radio" value="2"/>
 							<label class="star star-2" for="star-2"></label>
-							<input class="star star-1" id="star-1" type="radio" name="star"/>
+							<input class="star star-1" id="star-1" name="rating[]" type="radio" value="1"/>
 							<label class="star star-1" for="star-1"></label>
+<!--							<input class="star star-0" id="star-0" name="rating[]" type="radio" value="0"/>-->
+<!--							<label class="star star-0" for="star-0"></label>-->
 						</fieldset>
+					</div>
+					<p class="description">Choose one or more rating levels, then click Submit.</p>
+					<div class="button-group">
+						<button type="submit" class="btn btn-primary">Submit</button>
+						<button type="reset" class="btn btn-default">Reset</button>
 					</div>
 				</div>
 			</form>
