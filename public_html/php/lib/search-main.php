96fbf79dd30a5e3a74c56963e9a97a51dcd4f5ed
TrevorRigler <tarigler@gmail.com>
2015-09-07 12:21:50 -0600

    adding trufork rating search to main search
    https://app.asana.com/0/42983347910415/45604655562203

--- public_html/php/lib/search-main.php
+++ public_html/php/lib/search-main.php
@@ -24,4 +24,29 @@
 					src="<?php echo $PREFIX; ?>php/controllers/global-text-search-controller.js"></script>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<form id="search-by-float-rating-form" method="get" action="<?php echo $PREFIX ?>php/controllers/search-by-float-rating-controller.php">
+				<div class="trufork-search-align">
+					<div class="box-rating">
+						<fieldset class="rating">
+							<input class="star star-5" id="star-5" type="radio" name="star"/>
+							<label class="star star-5" for="star-5"></label>
+							<input class="star star-4" id="star-4" type="radio" name="star"/>
+							<label class="star star-4" for="star-4"></label>
+							<input class="star star-3" id="star-3" type="radio" name="star"/>
+							<label class="star star-3" for="star-3"></label>
+							<input class="star star-2" id="star-2" type="radio" name="star"/>
+							<label class="star star-2" for="star-2"></label>
+							<input class="star star-1" id="star-1" type="radio" name="star"/>
+							<label class="star star-1" for="star-1"></label>
+						</fieldset>
+					</div>
+				</div>
+			</form>
+			<div id="outputArea"></div>
+			<script type="text/javascript"
+					  src="<?php echo $PREFIX; ?>php/controllers/search-by-float-rating-controller.js"></script>
+		</div>
+	</div>
 </div>
