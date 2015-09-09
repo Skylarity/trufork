fba8f01692db9b7554202bc330ea023b5b7ca42c
Trevor Rigler <tarigler@gmail.com>
2015-09-09 09:36:41 -0600

    trufork rating search now populates results as clickable links in same style as main search
    https://app.asana.com/0/41180108506050/45604655562203

--- public_html/php/controllers/search-by-float-rating-controller.php
+++ public_html/php/controllers/search-by-float-rating-controller.php
@@ -5,9 +5,18 @@
  * Searches restaurant class/table by trufork rating
  */
 
-require_once(dirname(__DIR__) . "/classes/restaurant.php");
+require_once(dirname(__DIR__) . "/classes/autoload.php");
 //require_once(dirname(__DIR__) . "/lib/xsrf.php");
 require_once("/etc/apache2/data-design/encrypted-config.php");
+/**
+ * Get the relative path.
+ * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/header.php FarmToYou Header
+ **/
+require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+$CURRENT_DEPTH = substr_count(__DIR__, "/");
+$ROOT_DEPTH = substr_count($ROOT_PATH, "/");
+$DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
+$PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 
 try {
 //	var_dump($_GET);
@@ -17,9 +26,9 @@ try {
 	}
 
 // verify the XSRF challenge; commented out bc this isn't a POST and there is no real user input
-	//if(session_status() !== PHP_SESSION_ACTIVE) {
-	//session_start();
-	//}
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+	}
 	//verifyXsrf();
 
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/trufork.ini");
@@ -33,36 +42,28 @@ try {
 	 * but this case offers no alternatives
 	 **/
 	$echoChamber = [];
-	$matchedRestaurants = [];
 	$noneFound = false;
 	for($i = 5; $i >= 0; $i--) {
 		if(in_array($i, $_GET["rating"]) === true) {
 			$result = Restaurant::getRestaurantsByForkRating($pdo, $i, $i + 1);
 			$echoChamber = array_merge($echoChamber, $result->toArray());
-			$matchedRestaurants[] = $result;
-			if(count($result) > 0) {
-//				echo implode($result);
-			} else {
-				$noneFound = true;
-			}
 		}
-	}
-	if($noneFound) {
-		echo "No restaurants were found with that TruFork rating.";
 	}
 
 //	var_dump($echoChamber);
-	foreach($echoChamber as $restaurant) {
-		echo "<ul>" . PHP_EOL .
-			"<li>" . $restaurant->getRestaurantId() . "</li>" . PHP_EOL . /** do we need this to populate w clickable links? */
-			"<li>" . $restaurant->getName() . "</li>" . PHP_EOL .
-			"<li>TruFork Rating = " . $restaurant->getForkRating() . "</li>" . PHP_EOL .
-			"<li>" . $restaurant->getAddress() . "</li>" . PHP_EOL .
-			"</ul>" . PHP_EOL;
+//	foreach($echoChamber as $restaurant) {
+//		echo "<ul>" . PHP_EOL .
+//			"<li>" . $restaurant->getRestaurantId() . "</li>" . PHP_EOL . /** do we need this to populate w clickable links? */
+//			"<li>" . $restaurant->getName() . "</li>" . PHP_EOL .
+//			"<li>TruFork Rating = " . $restaurant->getForkRating() . "</li>" . PHP_EOL .
+//			"<li>" . $restaurant->getAddress() . "</li>" . PHP_EOL .
+//			"</ul>" . PHP_EOL;
 //			'<a href="php/lib/restaurant.php?id='.$result['restaurantId'].'">'.$result['name'].'</a>';
-	}
+//	}
 
-	$_SESSION["matchedRestaurants"] = $matchedRestaurants;
+//	var_dump($PREFIX);
+	$_SESSION["matchedRestaurants"] = $echoChamber;
+	header("Location: ${PREFIX}results", true, 301);
 
 	// ???
 	// profit!
